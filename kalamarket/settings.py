a1a80590c026554ebd177605f7b1e9eed5458121
zdhqan0108-cpu <zdhqan.0108@gmail.com>
2026-06-28 00:50:44 +0330

    fix lightGallery

--- kalamarket/settings.py
+++ kalamarket/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
